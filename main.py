83c7a1d5309cef6fcd6bdebaf4050c33e155d325
Mirpas <36613633+DeimosDesktop@users.noreply.github.com>
2022-02-01 16:27:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+#############################################
+## Visita mirpas.com para más información. ##
+#############################################
+
+#Importar las clases necesarias unicamente
 from flask import Flask, render_template, request, url_for, redirect
 import db
 from models import Tarea
 
+#Inicializa el servidor local
 app = Flask(__name__)
 
+#Función para la página principal
 @app.route("/")
 def saludo():
     tareas = db.session.query(Tarea).all()
     for t in tareas:
         print(t)
     return render_template("index.html", lista_tareas = tareas)
+    
+@app.route("/crear-tarea", methods=["post"])
+def crear():
+    tarea = Tarea(contenido=request.form['contenido_tarea'], hecha=False)
+    db.session.add(tarea)
+    db.session.commit()
+    return redirect(url_for('saludo'))
 
 if __name__ == "__main__":
     db.Base.metadata.create_all(db.server)
     app.run(debug=True)
+
